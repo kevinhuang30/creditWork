4bc14b99c9eb0c8ee675416421403ff39889da8b
kevin <huangxin3000@gmail.com>
2021-04-12 23:59:18 +1200

    my project

---
+++ CreditWorks/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecuredSigning.Startup))]
+namespace SecuredSigning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
